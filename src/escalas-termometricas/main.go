bdaf0c3375302e2862dd172164c0f746e4ad43d2
davimurilo <67333245+davimurilo@users.noreply.github.com>
2026-04-22 23:52:01 -0300

    Ajustes Finais

--- src/escalas-termometricas/main.go
+++ src/escalas-termometricas/main.go
@@ -4,14 +4,8 @@ import "fmt"
 
 func main() {
 
-	temperaturaCelsius := 100.0
-	temperaturaFahrenheit := 100.0
+	temperaturaKelvin := 100.00
+	temperaturaKelvinParaCelsius := temperaturaKelvin - 273.15
 
-	temperaturaCelsiusParaFahrenheit := ((9 * temperaturaCelsius) / 5) + 32
-	temperaturaFahrenheitParaCelsius := ((temperaturaFahrenheit - 32) * 5) / 9
-
-	fmt.Printf("A temperatura %f em Celsius, será %f em Fahrenheit.", temperaturaCelsius, temperaturaCelsiusParaFahrenheit)
-	fmt.Println()
-	fmt.Printf("A temperatura %f em Fahrenheit, será %f em Celsius.", temperaturaFahrenheit, temperaturaFahrenheitParaCelsius)
-	fmt.Println()
+	fmt.Printf("A temperatura de %.2f K (Kelvin) é igual a %.2f °C (Celsius)", temperaturaKelvin, temperaturaKelvinParaCelsius)
 }
